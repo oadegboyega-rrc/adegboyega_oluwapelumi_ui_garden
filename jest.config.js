682ce35002c34be295840fa9bd57352951f31164
oadegboyega-rrc <oadegboyega@academic.rrc.ca>
2025-07-23 16:00:34 -0500

    Ignore eslint

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
+const { pathsToModuleNameMapper, createDefaultPreset } = require('ts-jest');
 
-import { pathsToModuleNameMapper } from 'ts-jest/utils';
-import { createDefaultPreset } from 'ts-jest';
-
-/** @type {import("jest").Config} **/
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
 };
